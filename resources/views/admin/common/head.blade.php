d6184bb0c0ada3133fc61b6681958f94a8af988c
arvindverma63 <arvindverma630635@gmail.com>
2025-11-23 10:42:10 +0530

    update

--- resources/views/admin/common/head.blade.php
+++ resources/views/admin/common/head.blade.php
@@ -1,6 +1,8 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=0.9, maximum-scale=1.0">
+
+
 <title>{{ $title ?? 'Admin Dashboard' }}</title>
 
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('admin/images/favicon.png') }}">
@@ -21,3 +23,8 @@
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
 <!-- MDB -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/9.2.0/mdb.min.css" rel="stylesheet" />
+<style>
+    html {
+        zoom: 0.90;
+    }
+</style>
