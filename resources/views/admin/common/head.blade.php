1bd1fe7980bd37dec673efddeffb5f27385e9c15
arvindverma63 <arvindverma630635@gmail.com>
2025-11-23 10:37:05 +0530

    Update

--- resources/views/admin/common/head.blade.php
+++ resources/views/admin/common/head.blade.php
@@ -21,9 +21,3 @@
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
 <!-- MDB -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/9.2.0/mdb.min.css" rel="stylesheet" />
-<style>
-    body {
-        max-width: 90%;
-        margin: 0 auto !important;
-    }
-</style>
