20e38cf7478b1fa0d64de1f5bca47ba1e2b0d06a
arvindverma63 <arvindverma630635@gmail.com>
2026-01-09 21:57:26 +0530

    update

--- resources/views/admin/pages/bids/index.blade.php
+++ resources/views/admin/pages/bids/index.blade.php
@@ -216,6 +216,8 @@
 
     @push('scripts')
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script>
             // --- Modal Logic ---
             const modal = document.getElementById('proposalModal');
