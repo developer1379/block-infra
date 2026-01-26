a55b5191822e1c73e802029b504883fdbcebbedd
arvindverma63 <arvindverma630635@gmail.com>
2026-01-26 13:43:53 +0530

    update

--- Modules/User/routes/web.php
+++ Modules/User/routes/web.php
@@ -9,8 +9,7 @@ Route::middleware(['web', 'auth', 'role:user'])->group(function () {
     Route::get('user/dashboard', [UserController::class, 'index'])->name('admin.user');
     Route::prefix('user')->as('user.')->group(function () {
         Route::resource('projects', ProjectController::class);
+        Route::post('/milestones/{milestone}/comments', [MilestoneCommentController::class, 'store'])
+            ->name('milestones.comments.store');
     });
-
-    Route::post('/milestones/{milestone}/comments', [MilestoneCommentController::class, 'store'])
-        ->name('milestones.comments.store');
 });
